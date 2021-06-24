784a4a2b5e94dd18294566cfa1eef35648d60004
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-06-24 13:42:00 +0100

    setup test database

--- spec/features/viewing_bokmarks_spec.rb
+++ spec/features/viewing_bokmarks_spec.rb
@@ -1,14 +1,14 @@
 feature 'Viewing bookmarks' do
-  scenario 'visiting the index page' do
-    visit('/')
-    expect(page).to have_content('Bookmark Manager')
-  end
-
   # As a time-pressed user
   # So that I can quickly go to web sites I regularly visit
   # I would like to see a list of bookmarks
 
   scenario 'A user can see bookmarks' do
+    con = PG.connect(dbname: 'bookmark_manager_test')
+    con.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+    con.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+    con.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
     visit('/bookmarks')
 
     expect(page).to have_content "http://www.makersacademy.com"
