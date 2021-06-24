1425b8acf383b577e5ebe93e0a8dbbf1464bce1f
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-06-24 09:48:14 +0100

    initial project setup

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class BookmarkManager < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Bookmark Manager'
+  end
+
+end
