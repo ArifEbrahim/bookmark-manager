29c113dc86a8732ba394f90b0c5efbda9bc2c0f9
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-06-24 11:13:59 +0100

    bookmarks can be viewed

--- spec/features/viewing_bokmarks_spec.rb
+++ spec/features/viewing_bokmarks_spec.rb
@@ -3,4 +3,16 @@ feature 'Viewing bookmarks' do
     visit('/')
     expect(page).to have_content('Bookmark Manager')
   end
+
+  # As a time-pressed user
+  # So that I can quickly go to web sites I regularly visit
+  # I would like to see a list of bookmarks
+
+  scenario 'A user can see bookmarks' do
+    visit('/bookmarks')
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
+  end
 end
